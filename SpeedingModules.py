2b7ce850b7e7d6517ee41684a6a92f2cc329c043
ItzCino <zhifengchen03@gmail.com>
2021-03-24 23:18:52 +1300

    more format checking

--- SpeedingModules.py
+++ SpeedingModules.py
@@ -13,15 +13,14 @@ defaultFinesData = ["1-10-30\n",
                     "31-35-300\n",
                     "36-40-400\n",
                     "41-45-510\n",
-                    "46-50-630"]
+                    "46-50-630"
+                    ]
+
 
 tunnelLength = 2690
 speedLimit = 80
-#warningSpeed = 7 # in km/hr
-#warningTimeLimit = int((tunnelLength / ((warningSpeed*1000)/3600))/60)
-warningTimeLimit = 15
+warningTimeLimit = 30
 warningSpeed = int(((tunnelLength / (warningTimeLimit*60))*3.6))
-print(warningSpeed)
 totalTunnelZone = tunnelLength
 
 
@@ -42,7 +41,7 @@ def finesFileCheck():
                         defaultFinesFile.write(fineRanges)
                 defaultFinesFile.close()
                 speedFinesData = open("fine_rates.txt", "r")
-                print('WARNING: A FINE RATES TIME DOES NOT EXSIST A DEFAULT "FINE RATES" FILE WILL BE CREATED!!!!')
+                print('WARNING: A FINE RATES TIME DOES NOT EXSIST!!! A DEFAULT "FINE RATES" FILE WILL BE AUTOMATICALLY CREATED!!!!')
                 time.sleep(2)
                 return speedFinesData
 
@@ -80,6 +79,7 @@ def speedFileCheck():
         while True:
                 try:
                         speedFileName = input("\nPlease input the name of the data file: ")
+                        print()
                         if speedFileName == "" or speedFileName == " ":
                                 print("WARNING!!! : PLEASE DO NOT LEAVE BLANK!!!\n")
                                 continue
@@ -104,33 +104,43 @@ def speedFileFormatCheck(speedFileName):
                         speedFileData = open(speedFileName, "r")
 
                         dataLines = speedFileData.readlines()
-                        for data in dataLines:
+                        for lineData in dataLines:
                                 speedFileLineCounter += 1
-                                if data == "\n":
+                                if lineData == "\n" or lineData == "" or lineData == " ":
                                         continue
-                                dataKey, dataValue = data.split(",")
-                                if dataKey in errorCarData:
+                                if lineData in errorCarData:
+                                        continue                                
+                                try:    
+                                        data = lineData.replace(" ","")
+                                        dataKey, dataValue = data.split(",")
+                                        dataValue = ((dataValue.split("\n"))[0])
+                                        temp1, temp2, temp3 = dataValue.split(":")
+                                        checkValue = dataValue.replace(":", "")
+                                except:
+                                        print("iWARNING!!! Error occurred on line {}: {}".format(speedFileLineCounter, data))
+                                        speedFormatError = True
+                                        errorCarData.append(lineData)  
                                         continue
-                                dataValue = ((dataValue.split("\n"))[0])
-                                checkValue = dataValue.replace(":", "")
-                                if int(checkValue) is False:
+                                if (int(checkValue) is False) or len(checkValue) != 6:
                                         print("WARNING!!! Error occurred on line {}: {}".format(speedFileLineCounter, data))
                                         speedFormatError = True
                                         errorCarData.append(dataKey)
+                                        continue
                                 if dataKey not in dataDict.keys():
                                         dataKey = dataKey
                                         dataDict.setdefault(dataKey, [])
                                         dataDict[dataKey].append(dataValue)
                                 else:
                                         dataDict[dataKey].append(dataValue)
+                                
                         return dataDict, speedFormatError
                 except:
-                        print("\nExcept: WARNING!!! Error occurred on line {}: {}".format(speedFileLineCounter, data))
+                        print("\nExcept: WARNING!!! Error occurred on line {}: {}".format(speedFileLineCounter, lineData))
+                        #print(errorCarData)
                         speedFormatError = True
-                        try:
-                                errorCarData.append(dataKey)
-                        except:
-                                return dataDict, speedFormatError
+                        speedFileLineCounter = 0
+                        errorCarData.append(lineData)
+
 
 def continueOrExitForFines():
         print("#=#=#=#=# !!!PLEASE READ!!! #=#=#=#=#\n")
@@ -144,8 +154,8 @@ def continueOrExitForFines():
                 try:
                         print("\nWould you like to continue or exit the program")
                         print("WARNING!: Continuing may not work as intended! ie missing values / calculations")
-                        continueState = (input("Type EXIT to exit or CONTINUE to continue the program: ")).lower()
-                        if continueState == "exit" or continueState == "e":
+                        continueState = (input("Type STOP to exit or CONTINUE to continue the calculations: ")).lower()
+                        if continueState == "stop" or continueState == "s":
                                 return True
                         if continueState == "continue" or continueState == "c":
                                 return False
@@ -166,22 +176,22 @@ def speedDataFormatError():
                 try:
                         print("\nWould you like to continue or exit the program")
                         print("WARNING!: Continuing may not work as intended! ie entries / calculations")
-                        continueState = (input("Type EXIT to exit or CONTINUE to continue the program: ")).lower()
-                        if continueState == "exit" or continueState == "e":
-                                return True
-                        if continueState == "continue" or continueState == "c":
+                        continueState = (input("Type STOP to exit or CONTINUE to continue the calculations: ")).lower()
+                        if continueState == "stop" or continueState == "s":
                                 return False
+                        if continueState == "continue" or continueState == "c":
+                                return True
                         if continueState == "" or continueState == " ":
                                 print("Nothing was entered\n")
                 except:
                         print("invalid input")
 
-def continueOrExitDueToFormat(continueState):
-        if continueState is True:
-                print("\nExiting Program . . .")
-                time.sleep(2)
-                sys.exit()
+def exitDueToFormat(continueState):
         if continueState is False:
+                print("\nOperation Cancelled! . . .")
+                time.sleep(2)
+                
+        if continueState is True:
                 print("\nContinuing Program: WARNING!: Continuing may not work as intended!\n")
                 time.sleep(2)
                 
@@ -189,13 +199,20 @@ def continueOrExitDueToFormat(continueState):
 def calculateSpeedPerHr(dataDict):
         for data in dataDict.keys():
                 if len(dataDict[data]) < 2:
+                        #if len(dataDict[data]) == 1:
+                                #print("WARNING!!! CAR: {} IS MISSING AND EXIT / ENTRY TIME".format(dataDict[data]))
+                                #continue                        
                         if len(dataDict[data]) == 0:
                                 print("WARNING!!! Car with unknown Plate has entered/exited at".format(dataDict[data]))
                                 continue
-                        print("WARNING!!! Car: {} has not been reported to have exited tunnel!!!".format(data))
+                        print("WARNING!!! Car: {} has been reported to NOT have exited tunnel OR DATA IS INCORRECT!!!".format(data))
                         continue
+                
                 entryTime = ((dataDict[data])[0])
                 exitTime = ((dataDict[data])[1])
+                if entryTime == exitTime:
+                        print("{} WARNING!!! : ERROR: ENTRY TIME IS THE SAME AS THE EXIT TIME! ".format(data))
+                        continue
                 entryTimeSplit = entryTime.split(":")
                 exitTimeSplit = exitTime.split(":")
                 entryTimeInSec = timedelta(hours=int((entryTimeSplit[0])), minutes=int((entryTimeSplit[1])), seconds=int((entryTimeSplit[2])))
@@ -208,7 +225,7 @@ def calculateSpeedPerHr(dataDict):
                 
         return carSpeeds
 
-def calculateFines(carSpeeds):
+def calculateFines(carSpeeds, maxSpeed, maxFine):
         
         for carPlate in carSpeeds.keys():
                 for speedRange in speedFines:
@@ -222,44 +239,44 @@ def calculateFines(carSpeeds):
                 if (speed + 80) <= warningSpeed:
                         print("WARNING!!! Car: {} has was in the tunnel for more than {} minutes".format(carPlate, warningTimeLimit))  
                 if (speed <= 0):
-                        carFine = 0
-                        print("{} speed is {} km/hr so fine is ${}".format(carPlate, carSpeeds[carPlate], carFine))
+                        carFine = None
+                        print("{} speed is {} km/hr so fine is {}".format(carPlate, carSpeeds[carPlate], carFine))
                 if speed > maxSpeed:
                         print("{} speed is {} km/hr so fine is ${}".format(carPlate, carSpeeds[carPlate], maxFine))
                 continue
                 
 
-speedFinesData = finesFileCheck()
-speedFines, formattingErrorForFineRates = finesFileFormatCheck(speedFinesData)
+#speedFinesData = finesFileCheck()
+#speedFines, formattingErrorForFineRates = finesFileFormatCheck(speedFinesData)
 
-maxSpeed = ((speedFines[-1])[1])
-maxFine = ((speedFines[-1])[2])
+#maxSpeed = ((speedFines[-1])[1])
+#maxFine = ((speedFines[-1])[2])
 
-if formattingErrorForFineRates is True:
-        # include a option to simply create a new fine rates file in program
-        continueState = continueOrExitForFines()
-        continueOrExitDueToFormat(continueState)
+#if formattingErrorForFineRates is True:
+        ## include a option to simply create a new fine rates file in program
+        #continueState = continueOrExitForFines()
+        #continueOrExitDueToFormat(continueState)
 
-speedFileName = speedFileCheck()
-dataDict, speedFormatError = speedFileFormatCheck(speedFileName)
-if speedFormatError is True:
-        continueState = speedDataFormatError()
-        continueOrExitDueToFormat(continueState)
+#speedFileName = speedFileCheck()
+#dataDict, speedFormatError = speedFileFormatCheck(speedFileName)
+#if speedFormatError is True:
+        #continueState = speedDataFormatError()
+        #continueOrExitDueToFormat(continueState)
 
-carSpeeds = calculateSpeedPerHr(dataDict)
-calculateFines(carSpeeds)
+#carSpeeds = calculateSpeedPerHr(dataDict)
+#calculateFines(carSpeeds)
 
-print("\n Terminal will exit in 10 seconds . . .")
-time.sleep(5)
-print("\n Terminal will exit in 5 seconds . . .")
-time.sleep(1)
-print("\n Terminal will exit in 4 seconds . . .")
-time.sleep(1)
-print("\n Terminal will exit in 3 seconds . . .")
-time.sleep(1)
-print("\n Terminal will exit in 2 seconds . . .")
-time.sleep(1)
-print("\n Terminal will exit in 1 seconds . . .")
-time.sleep(1)
-print("\n Exiting . . .")
-time.sleep(1)
+#print("\n Terminal will exit in 10 seconds . . .")
+#time.sleep(5)
+#print("\n Terminal will exit in 5 seconds . . .")
+#time.sleep(1)
+#print("\n Terminal will exit in 4 seconds . . .")
+#time.sleep(1)
+#print("\n Terminal will exit in 3 seconds . . .")
+#time.sleep(1)
+#print("\n Terminal will exit in 2 seconds . . .")
+#time.sleep(1)
+#print("\n Terminal will exit in 1 seconds . . .")
+#time.sleep(1)
+#print("\n Exiting . . .")
+#time.sleep(1)
